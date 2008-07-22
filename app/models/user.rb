5f1cff0ca233c76f49bdabe8d7fb8dfb8fff290e
yvon <yvon@def688e0-9529-4944-9651-9dcdbfdb7c57>
2008-07-22 11:14:57 +0000

    Attr accessibles
    git-svn-id: svn://think-alternative.net/artic/trunk@25 def688e0-9529-4944-9651-9dcdbfdb7c57

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :password, :password_confirmation
+  # attr_accessible :login, :email, :password, :password_confirmation
 
 
 
