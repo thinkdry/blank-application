d10f4b19ccec6c916b327f09ac87f4371e66e158
yvon <yvon@def688e0-9529-4944-9651-9dcdbfdb7c57>
2008-07-22 06:32:47 +0000

    Plugins: FileColumn && RestfulAuth 'User' model 'sessions' and 'users' controllers
    git-svn-id: svn://think-alternative.net/artic/trunk@16 def688e0-9529-4944-9651-9dcdbfdb7c57

---
+++ db/migrate/20080722051438_create_users.rb
@@ -0,0 +1,33 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table "users", :force => true do |t|
+      t.column :login,                      :string, :limit => 40
+      t.column :firstname,                  :string, :limit => 255
+      t.column :lastname,                   :string, :limit => 255
+      t.column :email,                      :string, :limit => 255
+      t.column :addr,                       :string, :limit => 500
+      t.column :laboratory,                 :string, :limit => 255
+      t.column :phone,                      :string, :limit => 255
+      t.column :mobile,                     :string, :limit => 255
+      t.column :activity,                   :string, :limit => 255
+      t.column :edito,                      :text
+      # CHANGED: FileColumn instead of Attachment FU
+      t.column :image_path,                 :strint, :limit => 500
+      
+      t.column :crypted_password,           :string, :limit => 40
+      t.column :salt,                       :string, :limit => 40
+     
+      t.timestamps
+     
+      t.column :remember_token,             :string, :limit => 40
+      t.column :remember_token_expires_at,  :datetime
+
+
+    end
+    add_index :users, :login, :unique => true
+  end
+
+  def self.down
+    drop_table "users"
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20080722051438) do
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "addr",                      :limit => 500
+    t.string   "laboratory"
+    t.string   "phone"
+    t.string   "mobile"
+    t.string   "activity"
+    t.text     "edito"
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
+
+end
