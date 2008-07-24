b0a480427f8e2e4b7c22ede70108dafdfc4ecb03
francois <francois@def688e0-9529-4944-9651-9dcdbfdb7c57>
2008-07-24 09:32:56 +0000

    looking for good forms ...
    git-svn-id: svn://think-alternative.net/artic/trunk@43 def688e0-9529-4944-9651-9dcdbfdb7c57

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
+	
   acts_as_ajax_validation
 
   make_resourceful do
