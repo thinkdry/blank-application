999be07f5b0d2bc260a51da2c3de89c4bc820679
yvon <yvon@def688e0-9529-4944-9651-9dcdbfdb7c57>
2008-08-04 10:21:23 +0000

    ..
    git-svn-id: svn://think-alternative.net/artic/trunk@77 def688e0-9529-4944-9651-9dcdbfdb7c57

--- lib/acts_as_item.rb
+++ lib/acts_as_item.rb
@@ -1,4 +1,24 @@
 module ActsAsItem
+  module Controllers
+    class ItemController < ApplicationController
+      acts_as_ajax_validation
+
+    	make_resourceful do
+        actions :all
+    		belongs_to :workspace
+
+        before :create, :new, :index do
+      	  permit "member of workspace" if @workspace
+      	end
+
+      	before :edit, :update, :delete do
+      	  debugger
+      	  permit "author of artic_file"
+    	  end
+      end
+    end
+  end
+  
   module ModelMethods
     
     def self.included(base)
@@ -11,12 +31,7 @@ module ActsAsItem
       end
     end
     
-    module InstanceMethods      
-      def before_validation_on_create
-        debugger
-        self.user = @current_user
-      end
-      
+    module InstanceMethods
       def associated_workspaces= workspace_ids
     		self.workspaces.delete_all
     		workspace_ids.each { |w| self.items.build(:workspace_id => w) }
