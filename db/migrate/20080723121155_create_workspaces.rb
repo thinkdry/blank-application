5441a5a5cacc24cfd73491860aafb8153a5087d0
yvon <yvon@def688e0-9529-4944-9651-9dcdbfdb7c57>
2008-08-25 11:16:50 +0000

    Acts as state machine not required
    git-svn-id: svn://think-alternative.net/artic/trunk@127 def688e0-9529-4944-9651-9dcdbfdb7c57

--- db/migrate/20080723121155_create_workspaces.rb
+++ db/migrate/20080723121155_create_workspaces.rb
@@ -2,6 +2,7 @@ class CreateWorkspaces < ActiveRecord::Migration
   def self.up
     create_table :workspaces do |t|
 			t.integer :creator_id
+			t.text    :description
       t.string :name
 
       t.timestamps
