d34ebcf3810aacf3638d40dca783b0b048fb62d9
yvon <yvon@def688e0-9529-4944-9651-9dcdbfdb7c57>
2008-09-08 15:07:30 +0000

    ...
    git-svn-id: svn://think-alternative.net/artic/trunk@173 def688e0-9529-4944-9651-9dcdbfdb7c57

--- lib/workers/pubmed_worker.rb
+++ lib/workers/pubmed_worker.rb
@@ -1,13 +1,14 @@
 class PubmedWorker < BackgrounDRb::MetaWorker
   set_worker_name :pubmed_worker
   def create(args = nil)
-    # this method is called when worker is loaded for the first time
+    add_periodic_timer(3600) { retrieve_items }
   end
   
   # TOTO: Auto
-  def retrieve_items pubmed_source
-    raise 'PubmedSource invalid' unless pubmed_source
-    pubmed_source.import_latest_items
+  def retrieve_items
+    PubmedSource.all.each do |s|
+      s.import_latest_items
+    end
   end
 end
 
