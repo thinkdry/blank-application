88e1352f29676fca206c37c52326f92ba3cbcffb
yvon <yvon@def688e0-9529-4944-9651-9dcdbfdb7c57>
2008-09-08 14:44:23 +0000

    Show of pubmed sources
    git-svn-id: svn://think-alternative.net/artic/trunk@172 def688e0-9529-4944-9651-9dcdbfdb7c57

--- app/controllers/pubmed_sources_controller.rb
+++ app/controllers/pubmed_sources_controller.rb
@@ -1,17 +1,11 @@
 # TODO: Associate author (user) on creation
 
-require 'pubmed'
-
 class PubmedSourcesController < ApplicationController
   acts_as_ajax_validation
   
 	make_resourceful do
     actions :all
 		belongs_to :workspace
-		
-		before :show do
-		  @rss = Pubmed.new(@current_object.url).rss
-	  end
   end
   
 end
