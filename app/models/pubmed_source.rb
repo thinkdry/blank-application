501ed43fd7f99ae585eeaf05598f28387e3b20a1
yvon <yvon@def688e0-9529-4944-9651-9dcdbfdb7c57>
2008-09-08 14:06:46 +0000

    PubmedWorker... working
    git-svn-id: svn://think-alternative.net/artic/trunk@171 def688e0-9529-4944-9651-9dcdbfdb7c57

--- app/models/pubmed_source.rb
+++ app/models/pubmed_source.rb
@@ -1,6 +1,32 @@
+require 'rss/1.0'
+require 'rss/2.0'
+require 'open-uri'
+
 class PubmedSource < ActiveRecord::Base
   belongs_to  :user
   has_many    :pubmed_items
   
   validates_presence_of :name, :url
+
+  def rss_content
+    return @rss if @rss
+    
+    content = String.new # raw content of rss feed will be loaded here
+    open(self.url) { |s| content = s.read }
+    @rss = RSS::Parser.parse(content, false)
+  end
+
+  def import_latest_items
+    rss_content.items.each do |item|
+      # Be sure that the item hasnt been imported before
+      if pubmed_items.count(:conditions => { :guid => item.guid.content }) <= 0
+        pubmed_items.create({
+          :guid           => item.guid.content,
+          :title          => item.title,
+          :description    => item.description,
+          :author         => item.author,
+          :link           => item.link })
+      end
+    end
+  end
 end
