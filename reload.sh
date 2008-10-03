1569c7c85177b6bced7ee8b4c18f7dbd9d1fd53c
root <root@ns203830.ovh.net>
2008-10-03 23:20:19 +0200

    New reload.sh

--- reload.sh
+++ reload.sh
@@ -1,1 +1,9 @@
-rake db:drop && rake db:migrate && rake db:fixtures:load
+rake db:drop && \
+rake db:create && \
+rake db:migrate && \
+rake db:fixtures:load && \
+rake backgroundrb:create_queue &&  \
+rake xapian:rebuild_index models="ArticFile Article Audio Image Publication Video" RAILS_ENV=development && \
+rake xapian:update_index RAILS_ENV=development && \
+./script/backgroundrb stop && \
+./script/backgroundrb start
