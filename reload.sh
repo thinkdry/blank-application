defe9ab801951f8eb429332bb7c5296c97a41203
Yvon Cognard <yvon.cognard@gmail.com>
2008-10-05 18:52:46 +0200

    BackrgoundDRB removed

--- reload.sh
+++ reload.sh
@@ -4,6 +4,5 @@ rake db:migrate && \
 rake db:fixtures:load && \
 rake backgroundrb:create_queue &&  \
 rake xapian:rebuild_index models="ArticFile Article Audio Image Publication Video" RAILS_ENV=development && \
-rake xapian:update_index RAILS_ENV=development && \
-./script/backgroundrb stop && \
-./script/backgroundrb start
+rake xapian:update_index RAILS_ENV=development
+
