71718b33d3af52de83822230cd1560bd86765de9
Yvon Cognard <yvon.cognard@gmail.com>
2008-10-08 18:50:45 +0200

    item_path helper moved to item_helper.rb to lib/acts_as_item/url_helpers.rb Included in application.rb and defined as helpers

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,8 +1,4 @@
 module ItemsHelper
-  def self.included base
-    define_prefixed_item_paths(base)
-  end
-  
   def item_rate(object, params = {})
     params = {
       :rerate => false,
@@ -134,37 +130,6 @@ module ItemsHelper
     link_to(object.title, item_path(object))
   end
   
-  def item_path object, params = {}
-    prefix = params.delete :prefix
-    
-    helper_name = String.new
-    helper_name += prefix + '_' if prefix
-    helper_name += 'workspace_' if current_workspace
-    helper_name += object.class.to_s.underscore + '_path'
-    
-    args = [object, params]
-    args.insert(0, current_workspace) if current_workspace
-
-    send helper_name, *args
-  end
-  
-  def new_item_path(model)
-    helper_name = 'new_'
-    helper_name += 'workspace_' if current_workspace
-    helper_name += model.to_s.underscore + '_path'
-    args = current_workspace ? [current_workspace] : []
-    send(helper_name, *args)
-  end
-  
-  def items_path(model)
-    model = model.table_name unless model.is_a?(String)  
-    if current_workspace
-      workspace_content_url(current_workspace.id, :page => model)
-    else
-      content_url(:page => model)
-    end
-  end
-  
   def display_tabs(page)
     content = String.new
     
@@ -194,18 +159,6 @@ module ItemsHelper
     collection = items.send(page, :order => 'created_at DESC')
     
     render(:partial => "items/item_in_list", :collection => collection.to_a)
-  end
-  
-  private
-  def self.define_prefixed_item_paths base
-    # OPTIMIZE: Import prefix list from a conf file
-     ['edit', 'rate', 'add_tag', 'remove_tag', 'comment'].each do |prefix|
-       base.send(:define_method, "#{prefix}_item_path") do |*args|
-         object, params = args[0], args[1] || {}
-         params[:prefix] = prefix
-         item_path(object, params)
-       end
-     end
   end
   
 end
