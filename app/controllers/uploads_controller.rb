bf604e36206b55e832152ce0ea8f53e6e45fe4d6
Yvon Cognard <yvon.cognard@gmail.com>
2008-10-11 18:02:53 +0200

    File upload controller

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,20 @@
+require 'FileUtils'
+
+class UploadsController < ApplicationController
+  def create
+    # TODO: Catch exeptions and return JS
+    begin
+      temp_path = params[:NewFile].path
+      original_filename = params[:NewFile].original_filename
+      
+      @dest_filename = "#{rand(1000)}_#{params[:NewFile].original_filename}"
+      @dest_url = File.join('/uploads', @dest_filename) 
+      @dest_path = File.join(RAILS_ROOT, 'public', @dest_url)
+      
+      FileUtils.copy(temp_path, @dest_path)
+      render :action => 'create', :layout => false
+    rescue Exception => e
+      render :action => 'create_failed', :layout => false
+    end
+  end
+end
--- public/fckeditor/fckconfig.js
+++ public/fckeditor/fckconfig.js
@@ -291,12 +291,12 @@ FCKConfig.FlashBrowserWindowWidth  = FCKConfig.ScreenWidth * 0.7 ;	//70% ;
 FCKConfig.FlashBrowserWindowHeight = FCKConfig.ScreenHeight * 0.7 ;	//70% ;
 
 FCKConfig.LinkUpload = true ;
-FCKConfig.LinkUploadURL = FCKConfig.BasePath + 'filemanager/connectors/' + _QuickUploadLanguage + '/upload.' + _QuickUploadExtension ;
+FCKConfig.LinkUploadURL = '/uploads';
 FCKConfig.LinkUploadAllowedExtensions	= ".(7z|aiff|asf|avi|bmp|csv|doc|fla|flv|gif|gz|gzip|jpeg|jpg|mid|mov|mp3|mp4|mpc|mpeg|mpg|ods|odt|pdf|png|ppt|pxd|qt|ram|rar|rm|rmi|rmvb|rtf|sdc|sitd|swf|sxc|sxw|tar|tgz|tif|tiff|txt|vsd|wav|wma|wmv|xls|xml|zip)$" ;			// empty for all
 FCKConfig.LinkUploadDeniedExtensions	= "" ;	// empty for no one
 
 FCKConfig.ImageUpload = true ;
-FCKConfig.ImageUploadURL = FCKConfig.BasePath + 'filemanager/connectors/' + _QuickUploadLanguage + '/upload.' + _QuickUploadExtension + '?Type=Image' ;
+FCKConfig.ImageUploadURL = '/uploads';
 FCKConfig.ImageUploadAllowedExtensions	= ".(jpg|gif|jpeg|png|bmp)$" ;		// empty for all
 FCKConfig.ImageUploadDeniedExtensions	= "" ;							// empty for no one
 
