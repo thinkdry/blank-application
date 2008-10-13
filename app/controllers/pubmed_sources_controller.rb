6203d0e6bc5f1014922a995c41d1e96632101eca
Yvon Cognard <yvon.cognard@gmail.com>
2008-10-13 17:50:50 +0200

    Paginate on PubmedSources fixed

--- app/controllers/pubmed_sources_controller.rb
+++ app/controllers/pubmed_sources_controller.rb
@@ -4,20 +4,26 @@ class PubmedSourcesController < ApplicationController
 	make_resourceful do
     actions :all
 		belongs_to :workspace
+
     before :create, :update do
       @current_object.user_id=@current_user
     end
+
     after :create do 
       # After addition of a source, import the RSS into DB.
       @current_object.import_latest_items
     end
+    
     before :index do
-      @current_object=PubmedSource.new
-			@current_objects=@current_objects.paginate(
-				:page => params[:page],
-				:order => :created_at,
-				:per_page => 2
-			)
+      # New object form displayed in index
+      @current_object = PubmedSource.new
     end
+  end
+  
+  def current_objects
+    @current_objects ||= PubmedSource.paginate(
+			:page => params[:page],
+			:order => :created_at
+		)
   end
 end
