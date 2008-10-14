c38a62207cbf5d751c2591d7039c79c3e00ed6bf
root <root@ns203830.ovh.net>
2008-10-14 16:37:11 +0200

    User creation fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,25 +40,6 @@ class UsersController < ApplicationController
     end
   end
  
-  def create
-    # logout_keeping_session!
-    @user = User.new(params[:user])
-    success = @user && @user.save
-    if success && @user.errors.empty?
-      # Protects against session fixation attacks, causes request forgery
-      # protection if visitor resubmits an earlier form using back
-      # button. Uncomment if you understand the tradeoffs.
-      # reset session
-      # self.current_user = @user # !! now logged in
-      # redirect_back_or_default('/')
-      flash[:notice] = "Nouvel utilisateur créé"
-			redirect_to user_url(@user)
-    else
-      flash[:error]  = "Ce compte n'a pu être créé, ressayez ou contactez l'administrateur."
-      render :action => 'new'
-    end
-  end
-  
   def current_objects
      conditions = if params['login']
        ["login LIKE :login OR firstname LIKE :login OR lastname LIKE :login", 
