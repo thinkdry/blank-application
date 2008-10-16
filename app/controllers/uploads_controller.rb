2cea59f9139f27bbfd787a95b1b4bdf83022114b
Yvon Cognard <yvon.cognard@gmail.com>
2008-10-16 21:43:18 +0200

    Using File.write instead of FileUtils.copy to upload files

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,19 +1,16 @@
-require 'fileutils'
-
 class UploadsController < ApplicationController
   def create
     begin
-      temp_path = params[:NewFile].path
       original_filename = params[:NewFile].original_filename
       
       @dest_filename = "#{rand(1000)}_#{params[:NewFile].original_filename}"
       @dest_url = File.join('/uploads', @dest_filename) 
       @dest_path = File.join(RAILS_ROOT, 'public', @dest_url)
       
-      FileUtils.copy(temp_path, @dest_path)
+      File.open(@dest_path, "wb") { |f| f.write(params[:NewFile].read) }
       render :action => 'create', :layout => false
     rescue Exception => e
-      logger.error(e)
+      logger.error(e.to_s + "\n" + e.backtrace.collect { |trace|' ' + trace + "\n" }.to_s)
       render :action => 'create_failed', :layout => false
     end
   end
