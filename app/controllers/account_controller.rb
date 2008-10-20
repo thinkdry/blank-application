631295dcd2c9c9df38e97365fcd9f89578deed56
Yvon Cognard <yvon.cognard@gmail.com>
2008-10-20 15:10:00 +0200

    Rspec and some tests Root view no longer includes elems user cant consult

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,9 +1,9 @@
 class AccountController < ApplicationController 
   # Root page ('/')
   def index
-    @latest_items = GenericItem.latest
+    @latest_items = GenericItem.consultable_by(current_user).latest
     @latest_users = User.latest
-    @latest_pubmed = PubmedItem.latest
+    @latest_pubmed = current_user.pubmed_items.latest
     @latest_ws = Workspace.latest
   end
 end
