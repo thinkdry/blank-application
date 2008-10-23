9610717f86fb4260b40fe2c2bbb7bf64a107e0df
yvon <yvon@def688e0-9529-4944-9651-9dcdbfdb7c57>
2008-10-23 16:07:18 +0000

    Image model validates presence of file_path
    git-svn-id: svn://think-alternative.net/artic/trunk@269 def688e0-9529-4944-9651-9dcdbfdb7c57

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,7 +4,8 @@ class Image < ActiveRecord::Base
   
   file_column :file_path, :magick => { :versions => { :thumb => "100x100", :web => "500x500" } }
   
-  validates_file_format_of :file_path, :in => ["gif", "jpeg", "jpg"]
+  validates_presence_of :file_path
+  validates_file_format_of :file_path, :in => ["png", "gif", "jpg"]
   
   def self.label
     "Image"
