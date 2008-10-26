a59dbc250946cb0cc934ac49989ada6557cdb86f
Yvon Cognard <yvon.cognard@gmail.com>
2008-10-26 22:55:56 +0100

    Reload.sh depreciated (capistrano power)

--- reload.sh
+++
@@ -1,7 +0,0 @@
-rake db:drop && \
-rake db:create && \
-rake db:migrate && \
-rake db:fixtures:load && \
-rake xapian:rebuild_index models="ArticFile Article Audio Image Publication Video" RAILS_ENV=development && \
-rake xapian:update_index RAILS_ENV=development && \
-rake cron:pubmed:update_rss
