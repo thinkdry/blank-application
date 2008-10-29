2c22e832f0a42706917a787e43995ee216581fee
Yvon Cognard <yvon.cognard@gmail.com>
2008-10-29 14:50:37 +0100

    Publications: repeatated lines removed during importation

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -4,10 +4,13 @@ class PublicationsController < ApplicationController
   acts_as_ajax_validation
   acts_as_item do
     before :new, :create do
+      # If the Publication is imported from Pubmed
+      # => Export values from the existing PubmedItem to the new Publication
       if params[:pubmed_item_id]
-         @pubmed_item = PubmedItem.find(params[:pubmed_item_id])
-        [:title, :description, :author, :link].each do |field|
-          @current_object.send("#{field}=", sanitize(@pubmed_item.send(field), :tags => %w()))
+        @pubmed_item = PubmedItem.find(params[:pubmed_item_id])
+        
+        %W(title description author link).each do |field|
+          @current_object.send("#{field}=", @pubmed_item.send(field))
         end
       end
     end
