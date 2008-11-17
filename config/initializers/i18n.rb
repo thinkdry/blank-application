25fd65c1927e759f6e985757894bb00f8f07bf29
François Turbelin <francois@thinkdry.com>
2008-11-17 21:48:46 +0530

    First step in i18n/l10n

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,18 @@
 #I18n.locale = 'nl-NL'
 #I18n.load_path << "#{RAILS_ROOT}/config/translations.yml"
 
-%w{yml rb}.each do |type|
-  I18n.load_path += Dir.glob("#{RAILS_ROOT}/app/locales/**/*.#{type}")
-end
-I18n.default_locale = 'LOL'
+
+I18n.default_locale = "fr-FR"
+#I18n.locale = 'fr-FR'
+#I18n.default_locale = 'fr-FR'
+#%w{yml rb}.each do |type|
+#  I18n.load_path += Dir.glob("#{RAILS_ROOT}/app/locales/*.#{type}")
+#end
+LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales"
+#LOCALES_AVAILABLE = Dir["#{LOCALES_DIRECTORY}/*.{rb,yml}"].collect do |locale_file|
+#  File.basename(File.basename(locale_file, ".rb"), ".yml")
+#end.uniq.sort
+#I18n.load_path += Dir.glob("#{LOCALES_DIRECTORY}/config/locales/*.yml")
+#I18n.load_path << "#{LOCALES_DIRECTORY}/fr-FR.yml"
+I18n.load_path << "#{LOCALES_DIRECTORY}/en-US.yml"
+I18n.load_path << "#{LOCALES_DIRECTORY}/fr-FR.yml"
