8512ea9d01ab70aa78fc4e34dffe26764d0028ad
Anup Nivargi <anupnivargi@gmail.com>
2008-11-28 16:31:46 +0530

    General Commit

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -10,30 +10,22 @@ class ElementsController < ApplicationController
   end
   
   def update
-    if !params[:newtemplate].blank? 
+    if !params[:newtemplate].blank?
        params[:template].each do |k_elmt, v_elmt|
       @element=Element.create(:name => k_elmt.to_s, :bgcolor=>v_elmt.to_s,:template=>params[:newtemplate])
+    end
       flash[:notice]="New Template Created"
-      end
-       redirect_to "/"
+      redirect_to "/"
     elsif params[:template]
       params[:template].each do |k_elmt, v_elmt|
       Element.find(:first, :conditions => {:name => k_elmt.to_s, :template => "current"}).update_attributes(:bgcolor => v_elmt.to_s)
       end
        flash[:notice]="Saved Sucessfully"
        redirect_to  "/"
-     else
+    else
        flash[:notice]="Changes not Saved"
-        render :action=> "/"
-      end
-    end  
-    #if @element.update_attributes(params[:element])
-      #  flash[:notice]="Saved Sucessfully"
-      
-      #else
-        # flash[:notice]="Changes not Saved"
-         #render :action => "/"
-      #end
-   #end
+       render :action=> "/"
+    end
+  end  
 end
 
