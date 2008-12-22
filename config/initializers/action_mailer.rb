1eb9fb04a566b6b172e4a110a0f7371bd332b854
François Turbelin <francois@thinkdry.com>
2008-12-22 14:51:30 +0530

    AM pb

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,8 +1,17 @@
 ActionMailer::Base.delivery_method = :smtp
 
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.think-alternative.net',
+		:domain => 'think-alternative.net',
+    :port => '25',
+		:user_name => 'test@think-alternative.net',
+		:password => 'test',
+		:authentication => :login
+}
+
 #ActionMailer::Base.smtp_settings = {
-#    :address => 'smtp.think-alternative.net',
-#		:domain => 'think-alternative.net',
+#    :address => 'smtp2.phpnet.org',
+#		:domain => 'phpnet.org',
 #    :port => '25',
 ##		:pop3_auth => {
 ##			:server => 'pop.think-alternative.net',
@@ -10,19 +19,10 @@ ActionMailer::Base.delivery_method = :smtp
 ##			:password => 'test',
 ##			:expires => 1.hour # expiration time for the credentials
 ##		}
-#		:user_name => 'test@think-alternative.net',
-#		:password => 'test',
+#		:user_name => 'blank@thinkdry.com',
+#		:password => 'blank',
 #		:authentication => :login
 #}
-
-ActionMailer::Base.smtp_settings = {
-    :address => 'smtp2.phpnet.org',
-		:domain => 'phpnet.org',
-    :port => '25',
-		:user_name => 'blank@thinkdry.com',
-		:password => 'blank',
-		:authentication => :login
-}
 
 ActionMailer::Base.perform_deliveries = true
 
