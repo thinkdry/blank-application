351c3b9a681575f71bedb67e816d0b59dd6be2cf
François Turbelin <francois@thinkdry.com>
2008-12-23 18:03:58 +0530

    Real to 1.0

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController 
   # Root page ('/')
   def index
-    @latest_items = GenericItem.consultable_by(current_user).latest
+    @latest_items = GenericItem.consultable_by(current_user.id).latest
     @latest_users = User.latest
     @latest_feeds = current_user.feed_items.latest
     @latest_ws = Workspace.latest
