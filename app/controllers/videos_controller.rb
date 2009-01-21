cebc9ef8a5850ef3b42ef1582ce05b77e93b2462
Anup Nivargi <anupnivargi@gmail.com>
2009-01-21 16:23:05 +0530

    UploadAtt

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,8 +4,16 @@ class VideosController < ApplicationController
      after :create do
          #MiddleMan.new_worker(:worker=>:converter_worker, :worker_key=>"encoder",:args=>{:type=>"video", :id => @current_object.id, :path=>params[:tmp][:video_path], :enc=>"flv"})
          if params[:tmp][:video_path]
-           session[:video_path]= params[:tmp][:video_path].path
-           p session[:video_path]
+           uri=URI.parse(params[:tmp][:video_path])
+           #session[:video_path]= params[:tmp][:video_path]
+           #p session[:video_path]
+           #p params[:tmp][:video_path]
+#           p ActionController::UploadedTempfile.new(params[:tmp][:video_path].path.split("/").last)
+#           p ActionController::UploadedTempfile.new(params[:tmp][:video_path]).methods.sort
+#           p ActionController::UploadedTempfile.new(params[:tmp][:video_path]).original_filename
+#           p ActionController::UploadedTempfile.new(params[:tmp][:video_path]).to_param
+#           p ActionController::UploadedTempfile.new(params[:tmp][:video_path]).to_tempfile()
+
            #session[:video_path]=params[:tmp][:video_path]
            #p session[:video_path]
 #         worker=MiddleMan.worker(:converter_worker)
