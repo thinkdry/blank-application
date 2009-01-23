ee7e02884795f00f49f346306f1c5b9d811ff20e
Anup Nivargi <anupnivargi@gmail.com>
2009-01-23 16:03:59 +0530

    general

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -12,9 +12,16 @@
 
 class Picture < ActiveRecord::Base
 	
-	file_column :picture_path, :magick => { :versions => { :favicon => "16x16", :thumb => "168x80", :normal => "400x80" } }
-  
+	#file_column :picture_path, :magick => { :versions => { :favicon => "16x16", :thumb => "168x80", :normal => "400x80" } }
+  has_attached_file :picture,
+                                   :url =>    "/uploaded_files/picture/:id/:style/:basename.:extension",
+                                   :path => ":rails_root/public/uploaded_files/picture/:id/:style/:basename.:extension",
+                                   :styles => { :medium => "480x80>",
+                                   :thumb => "16x16>" }
+  validates_attachment_presence :image
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+  validates_attachment_size(:image, :less_than => 2.megabytes)
   #validates_presence_of :picture_path, :allow_blank => true
-  validates_file_format_of :picture_path, :in => ["png", "gif", "jpg"]
+  #validates_file_format_of :picture_path, :in => ["png", "gif", "jpg"]
 	
 end
