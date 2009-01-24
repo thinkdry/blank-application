f62a7ac32835254412fffca5a81b02afc387d1eb
Anup Nivargi <anupnivargi@gmail.com>
2009-01-24 16:31:12 +0530

    SuperAdmin

--- db/migrate/20090124101629_favicons.rb
+++ db/migrate/20090124101629_favicons.rb
@@ -4,7 +4,6 @@ class Favicons < ActiveRecord::Migration
     add_column :pictures, :favicon_content_type, :string
     add_column :pictures, :favicon_file_size, :integer
     add_column :pictures, :favicon_updated_at, :datetime
-    Picture.create(:name=>"favicon")
   end
 
   def self.down
