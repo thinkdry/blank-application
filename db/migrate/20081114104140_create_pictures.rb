18f4c735eee39a6c955a87190a43043e701c494e
François Turbelin <francois@thinkdry.com>
2009-01-27 23:58:35 +0530

    test capi

--- db/migrate/20081114104140_create_pictures.rb
+++ db/migrate/20081114104140_create_pictures.rb
@@ -2,7 +2,10 @@ class CreatePictures < ActiveRecord::Migration
   def self.up
     create_table :pictures do |t|
       t.string :name
-      t.string :picture_path
+      t.string :picture_file_name
+			t.string :picture_content_type
+			t.integer :picture_file_size
+			t.datetime :picture_updated_at
       t.timestamps
     end
      Picture.create(:name => 'logo')
