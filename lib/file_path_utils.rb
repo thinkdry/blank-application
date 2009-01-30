97243260e697a9b876dfd31578d438ba4f6448ca
Anup Nivargi <anup@laptop.(none)>
2009-01-30 10:24:01 +0530

    FIxtures1

--- lib/file_path_utils.rb
+++ lib/file_path_utils.rb
@@ -6,8 +6,7 @@ def file_path_import(params)
     raise unless params[:file_path]
     
     src = File.join(RAILS_ROOT, 'test', 'fixtures', 'file_path', params[:model].to_s, params[:file_path].to_s)
-    dst = File.join(RAILS_ROOT, 'public', params[:model].to_s, 'file_path', params[:id].to_s)
-    
+    dst = File.join(RAILS_ROOT, 'public','uploaded_files', params[:model].to_s, params[:id].to_s, 'orignal', params[:cmsfile_file_name])
     FileUtils.ln_s(src, dst) unless File.exists?(dst)
-    params[:file_path]
+    params[:cmsfile_file_name]
 end
