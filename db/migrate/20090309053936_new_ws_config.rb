27b0450fbb7cf1ef8bc5a232d7daede041a70edf
François Turbelin <francois@thinkdry.com>
2009-03-13 13:07:48 +0530

    category and right 2

---
+++ db/migrate/20090309053936_new_ws_config.rb
@@ -0,0 +1,14 @@
+class NewWsConfig < ActiveRecord::Migration
+  def self.up
+    drop_table :ws_configs
+		remove_column :workspaces, :ws_config_id
+		add_column :workspaces, :ws_items, :string
+		add_column :workspaces, :ws_item_categories, :string
+		add_column :workspaces, :logo_file_name, :string
+    add_column :workspaces, :logo_content_type, :string
+    add_column :workspaces, :logo_file_size, :integer
+  end
+
+  def self.down
+  end
+end
