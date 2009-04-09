037d969ba4f48e584205fecbb0debd884e3a447f
François Turbelin <francois@thinkdry.com>
2009-04-09 14:27:21 +0530

    first debug

--- app/models/search.rb
+++ app/models/search.rb
@@ -56,4 +56,38 @@ class Search < ActiveRecord::Base
 		return res
 	end
 
+	def do_search
+		results = []
+		if self[:models].split(',').size == 1
+			# Research on ONE model
+			model_const = self[:models].split(',').first.classify.constantize
+			if !self[:full_text_field].blank? && (self[:full_text_field] != I18n.t('layout.search.search_label'))
+				results += model_const.full_text_with_xapian(self[:full_text_field]).advanced_on_fields(self.conditions).filtering_on_field(self[:filter_name], self[:filter_way], self[:filter_limit])
+			else
+				results += model_const.advanced_on_fields(self.conditions).filtering_on_field(self[:filter_name], self[:filter_way], self[:filter_limit])
+			end
+		else
+			# Research on VARIOUS models
+			self[:models].split(',').each do |model_name|
+				model_const = model_name.classify.constantize
+				if !self[:full_text_field].blank? && (self[:full_text_field] != I18n.t('layout.search.search_label'))
+					results += model_const.full_text_with_xapian(self[:full_text_field]).advanced_on_fields(self.conditions)
+				else
+					results += model_const.advanced_on_fields(self.conditions)
+				end
+				# Filer in Ruby, not top, need to find a MySQL way
+				if self[:filter_name] != 'weight'
+					results = results.sort do |x, y|
+						if (self[:filter_way] == 'desc')
+							x.send(self[:filter_name].to_sym) <=> y.send(self[:filter_name].to_sym)
+						else
+							y.send(self[:filter_name].to_sym) <=> x.send(self[:filter_name].to_sym)
+						end
+					end[0..self[:filter_limit].to_i] # TODO limit
+				end
+			end
+		end
+		return results
+	end
+
 end
