1e0622bc0fe6978ed4d7ff4e2998119537bdf22c
François Turbelin <francois@thinkdry.com>
2009-04-23 19:28:49 +0530

    small bug

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -123,7 +123,7 @@ class WorkspacesController < ApplicationController
   def ajax_show 
       params[:id] ||= params[:workspace_id]
 			params[:item_type] ||= (@current_object.ws_items.split(',') & @configuration['sa_items']).first.to_s.pluralize
-			@current_objects = get_item_list(params[:item_type])
+			@current_objects = get_items_list(params[:item_type])
 			@paginated_objects = @current_objects.paginate(:per_page => get_per_page_value, :page => params[:page])
       render :partial=>"items/tab_list", :layout=>false
     end
