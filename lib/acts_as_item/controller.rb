46901b9f87f68ad70f14585e0f2edce5efb70a57
Nagarjuna Rachaneni <nagarjuna@nagarjuna-laptop.(none)>
2009-06-01 15:49:12 +0530

    full path to images which uploaded from fck editor and remove body form newsletter form for new

--- lib/acts_as_item/controller.rb
+++ lib/acts_as_item/controller.rb
@@ -93,6 +93,10 @@ module ActsAsItem
 						format.atom { render :template => "#{params[:controller]}/index.atom.builder", :layout => false }
 	        end
 
+          response_for :destroy do |format|
+            format.html { redirect_to(items_path(params[:controller])) }
+          end
+          
 #					response_for :update do |format|
 #						#format.html { redirect_to item_path(@current_object)}
 #						format.html { redirect_to((ws=current_workspace) ? workspace_path(ws.id)+"/#{@current_object.class.to_s.underscore.pluralize}" : "/content/#{@current_object.class.to_s.underscore.pluralize}") }
