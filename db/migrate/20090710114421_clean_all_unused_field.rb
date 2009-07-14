be4b64f43ecd35127bad3525976bc1c5c56bfc4e
François Turbelin <francois@thinkdry.com>
2009-07-14 15:33:51 +0530

    page on migration ...

--- db/migrate/20090710114421_clean_all_unused_field.rb
+++ db/migrate/20090710114421_clean_all_unused_field.rb
@@ -2,13 +2,15 @@ class CleanAllUnusedField < ActiveRecord::Migration
   def self.up
     #remove_column :queued_mails, :user_id
     #remove_column :queued_mails, :url
-    (ITEMS - ['feed_source','bookmark', 'page']).each do |item|
-      remove_column item.pluralize.to_sym, :tags
-      remove_column item.pluralize.to_sym, :category
-    end
-    remove_column :bookmarks, :category
-    remove_column :feed_sources, :category
-		remove_column :pages, :category
+#    (ITEMS - ['feed_source','bookmark', 'page']).each do |item|
+#      remove_column item.pluralize.to_sym, :tags
+#      remove_column item.pluralize.to_sym, :category
+#    end
+#    remove_column :bookmarks, :category
+#    remove_column :feed_sources, :category
+#		if ITEMS.include?('page')
+#			remove_column :pages, :category
+#		end
   end
 
   def self.down
