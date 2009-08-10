16039093087f7cd25000e3a3d7d2d02d95e47b0f
François Turbelin <francois@thinkdry.com>
2009-08-10 18:03:15 +0530

    free user creation permission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 	def permission_checking
 		if params[:action] == 'new' || params[:action] == 'create'
 			build_object
-			no_permission_redirection unless @current_user && @current_object.send("accepts_new_for?".to_sym, @current_user)
+			no_permission_redirection unless (@current_user.nil? && is_allowed_free_user_creation?) || (@current_user && @current_object.send("accepts_new_for?".to_sym, @current_user))
 		elsif params[:action] == 'edit' || params[:action] == 'update'
 			current_object
 			no_permission_redirection unless @current_user && @current_object.send("accepts_edit_for?".to_sym, @current_user)
