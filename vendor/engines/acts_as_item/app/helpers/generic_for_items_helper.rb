b26544b25f85cf3cb3a8df595c24730ea1c00d32
Nagarjuna Rachaneni <nagarjuna@nagarjuna-laptop.(none)>
2009-08-31 19:13:05 +0530

    removed item resource from routes.rb

--- vendor/engines/acts_as_item/app/helpers/generic_for_items_helper.rb
+++ vendor/engines/acts_as_item/app/helpers/generic_for_items_helper.rb
@@ -118,11 +118,11 @@ module GenericForItemsHelper
     if current_workspace
         most_commented = GenericItem.from_workspace(current_workspace.id).most_commented.to_a
         best_rated = GenericItem.from_workspace(current_workspace.id).best_rated.to_a
-        feed_items = FeedItem.from_workspace(current_workspace.id)
+        feed_items = FeedItem.from_workspace(current_workspace.id).latest.to_a
     else
         most_commented = GenericItem.consultable_by(current_user.id).most_commented.to_a
         best_rated = GenericItem.consultable_by(current_user.id).best_rated.to_a
-        feed_items = FeedItem.consultable_by(current_user.id)
+        feed_items = FeedItem.consultable_by(current_user.id).latest.to_a
     end
     return render :partial => "generic_for_items/"+partial_name,
                 :locals =>{:most_commented => most_commented, :best_rated => best_rated, :feed_items => feed_items}
