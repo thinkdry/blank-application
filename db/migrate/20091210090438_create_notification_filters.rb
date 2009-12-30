f1f354dc972baa1cd8514ec39331c1695e0c2be8
thinkdry <contact@thinkdry.com>
2009-12-30 07:58:24 +0100

    Added missing translations for notifications

--- db/migrate/20091210090438_create_notification_filters.rb
+++ db/migrate/20091210090438_create_notification_filters.rb
@@ -5,6 +5,14 @@ class CreateNotificationFilters < ActiveRecord::Migration
       t.string :group
       t.timestamps
     end
+		ITEMS.each do |item|
+			NotificationFilter.create(:name => item, :group => 'model')
+		end
+
+		NotificationFilter.create(:name => 'destroy', :group => 'action')
+		NotificationFilter.create(:name => 'create', :group => '')
+		NotificationFilter.create(:name => 'update', :group => '')
+
   end
 
   def self.down
